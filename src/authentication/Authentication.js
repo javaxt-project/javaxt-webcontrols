bef156e081e935fe12bd7a772e48667714438d83
Peter Borissow <peter.borissow@yahoo.com>
2018-01-03 15:08:21 -0500

    Minor code clean-up.

--- src/authentication/Authentication.js
+++ src/authentication/Authentication.js
@@ -27,9 +27,6 @@ javaxt.dhtml.Authentication = function(loginURL, logoutURL) {
       //Instantiate HTTP Request
         var request = ((window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"));
         request.open("GET", loginURL, true, username, password);
-        request.send(null);
-
-      //Process Response
         request.onreadystatechange = function(){
             if (request.readyState === 4) {
                 if (request.status !== 200){
@@ -40,6 +37,7 @@ javaxt.dhtml.Authentication = function(loginURL, logoutURL) {
                 if (callback!=null) callback.call(request);
             }
         };
+        request.send(null);
     };
 
 
@@ -56,27 +54,25 @@ javaxt.dhtml.Authentication = function(loginURL, logoutURL) {
         }
         else{ 
 
-            var request1 = new XMLHttpRequest();
-            var request2 = new XMLHttpRequest();
-
           //Logout. Tell the server not to return the "WWW-Authenticate" header
-            request1.open("GET", logoutURL + "?prompt=false", true);
-            request1.send("");
-            request1.onreadystatechange = function(){
-                if (request1.readyState === 4) {
+            var request = new XMLHttpRequest();
+            request.open("GET", logoutURL + "?prompt=false", true);
+            request.onreadystatechange = function(){
+                if (request.readyState === 4) {
 
                   //Login with dummy credentials to clear the auth cache
+                    var request2 = new XMLHttpRequest();
                     request2.open("GET", loginURL, true, "logout", "logout");
-                    request2.send("");
-
                     request2.onreadystatechange = function(){
                         if (request2.readyState === 4) {
                             if (callback!=null) callback.call();
                         }
                     };
+                    request2.send("");
 
                 }
             };
+            request.send("");
         }
     };
 };
