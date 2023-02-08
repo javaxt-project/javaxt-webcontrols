3ec977fadbe4ae328c7d624bac76c77df2f46f0f
Peter Borissow <peter.borissow@yahoo.com>
2023-02-08 09:15:12 -0500

    Fixed bug setting the initial value in the Switch class

--- src/switch/Switch.js
+++ src/switch/Switch.js
@@ -2,10 +2,10 @@ if(!javaxt) var javaxt={};
 if(!javaxt.dhtml) javaxt.dhtml={};
 
 //******************************************************************************
-//**  Slider
+//**  Switch
 //*****************************************************************************/
 /**
- *   Basic toggle switch
+ *   Basic on/off toggle switch
  *
  ******************************************************************************/
 
@@ -13,7 +13,14 @@ javaxt.dhtml.Switch = function(parent, config) {
 
     var me = this;
     var defaultConfig = {
+
+      /** If true, the slider will be set to the on position.
+       */
         value: true,
+
+      /** Style for individual elements within the component. Note that you can
+       *  provide CSS class names instead of individual style definitions.
+       */
         style: {
             groove: {
                 backgroundColor: "#dcdcdc",
@@ -71,66 +78,77 @@ javaxt.dhtml.Switch = function(parent, config) {
       //Merge clone with default config
         merge(clone, defaultConfig);
         config = clone;
-        
-        
+
+
         groove = document.createElement("div");
         setStyle(groove, config.style.groove);
         parent.appendChild(groove);
         groove.onclick = function(){
             me.setValue(!me.getValue());
         };
-        
+
         handle = document.createElement("div");
         setStyle(handle, config.style.handle);
         groove.appendChild(handle);
-        
-        me.setValue(config.value);
+        me.setValue(config.value, true);
         me.el = groove;
-        
+
       //Add public show/hide methods
         addShowHide(me);
     };
-    
-    
+
+
   //**************************************************************************
   //** getValue
   //**************************************************************************
+  /** Returns true on the slider is set to the on/active position. Otherwise
+   *  returns false.
+   */
     this.getValue = function(){
         return value;
     };
-    
-    
+
+
   //**************************************************************************
   //** setValue
   //**************************************************************************
+  /** Used to set the slider to the on or off position.
+   *  @param b If true, sets the slider to the on position. Otherwise, the
+   *  slider will be set to the off position.
+   *  @param silent By default, this slider will fire the onChange event
+   *  whenever the value is changed. When silent is set to true, the slider
+   *  will NOT fire the onChange event. This parameter is optional.
+   */
     this.setValue = function(b, silent){
         if (b===true || b===false){
             if (b===value) return;
-            if (value===true){
-                setStyle(groove, config.style.groove);
-                setStyle(handle, config.style.handle);
-                handle.style.right = "";
-                handle.style.left = 0;
-            }
-            else{
+            if (b===true){
                 setStyle(groove, config.style.grooveActive);
                 setStyle(handle, config.style.handleActive);
                 handle.style.left = "";
                 handle.style.right = 0;
+            }
+            else{
+                setStyle(groove, config.style.groove);
+                setStyle(handle, config.style.handle);
+                handle.style.right = "";
+                handle.style.left = 0;
             }
             value = b;
             if (silent===true){}
             else me.onChange(value);
         }
     };
-    
-    
+
+
   //**************************************************************************
   //** onChange
   //**************************************************************************
-    this.onChange = function(){};
-    
-    
+  /** Called when the input value changes
+   */
+    this.onChange = function(value){};
+
+
   //**************************************************************************
   //** Utils
   //**************************************************************************
