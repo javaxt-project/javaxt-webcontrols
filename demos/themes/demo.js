999be8741a4f31ce311ddb756c0608a59f611f3c
Peter Borissow <peter.borissow@yahoo.com>
2024-04-19 16:02:11 -0400

    Added tabs to the themes demo

--- demos/themes/demo.js
+++ demos/themes/demo.js
@@ -4,23 +4,70 @@ var renderComponents = function(parent, style){
     var div = document.createElement("div");
     div.style.width = "100%";
     div.style.height = "100%";
+    div.style.position = "relative";
     parent.appendChild(div);
 
 
-//    javaxt.dhtml.utils.updateDOM();
-//    var createElement = javaxt.dhtml.utils.createElement;
-//    var createTable = javaxt.dhtml.utils.createTable;
+    javaxt.dhtml.utils.updateDOM();
+    var createElement = javaxt.dhtml.utils.createElement;
+    var createTable = javaxt.dhtml.utils.createTable;
 
 
+    var createTile = function(){
+        return javaxt.dhtml.utils.createElement("div", div, {
+            position: "relative",
+            width: "600px",
+            height: "400px",
+            float: "left"
+        });
+    };
 
-    var window = new javaxt.dhtml.Window(div, {
+
+  //Create window
+    var window = new javaxt.dhtml.Window(createTile(), {
         title: "Window",
         width: 400,
         height: 200,
-        style: style ? style.window : {}
+        style: style ? style.window : {},
+        movable: false,
+        resizable: false
     });
-
     window.show();
 
 
+
+  //Create tab panel
+    var tabContainer = javaxt.dhtml.utils.createElement("div", createTile(), {
+        width: "300px",
+        height: "300px"
+    });
+    tabContainer.className = "middle center";
+    var tabs = new javaxt.dhtml.TabPanel(tabContainer, {
+        style: style ? style.tabPanel : {}
+    });
+    tabs.addTab("Selected", "");
+    tabs.addTab("Search", "");
+    tabs.raiseTab(0);
+
+
+
+
+  //Create window with tabs
+    var window = new javaxt.dhtml.Window(createTile(), {
+        title: "Tab Window",
+        width: 400,
+        height: 200,
+        style: style ? style.window : {},
+        movable: false,
+        resizable: false
+    });
+    window.getBody().style.padding = 0;
+
+    var tabs = new javaxt.dhtml.TabPanel(window.getBody(), {
+        style: style ? style.tabPanel : {}
+    });
+    tabs.addTab("Selected", "");
+    tabs.addTab("Search", "");
+    tabs.raiseTab(0);
+    window.show();
 };
